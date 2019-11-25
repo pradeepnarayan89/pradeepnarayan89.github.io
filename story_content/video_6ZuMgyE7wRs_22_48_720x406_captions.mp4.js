5ac6861620435cfce076c17508c0c814c2e34abd
pradeepnarayan89 <pradeep@ittblazers.com>
2019-11-25 12:15:40 -0500

    Hugo Site

--- story_content/video_6ZuMgyE7wRs_22_48_720x406_captions.mp4.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.30.19518.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:01.696%0D%0ADid%20I%20answer%20all%20of%20your%20questions?%0D%0A%0D%0A"}')
